71c32b2ae7f9b210fa6d6177cc2fd912204b1464
ishandab <ishanbasnet76@gmail.com>
2025-09-30 17:44:06 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-// Detect scroll + activate the current slide
 const slides = document.querySelectorAll(".slide");
 
 function activateSlides() {
   slides.forEach(slide => {
     let rect = slide.getBoundingClientRect();
-    if (rect.top < window.innerHeight / 2 && rect.bottom > window.innerHeight / 2) {
+    if(rect.top < window.innerHeight/2 && rect.bottom > window.innerHeight/2){
       slide.classList.add("active");
     } else {
       slide.classList.remove("active");
